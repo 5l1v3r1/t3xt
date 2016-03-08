9de4c8c4c3619b5c4176a9be67c4c87a1d576bb6
Alex Nichol <unixpickle@gmail.com>
2016-03-08 18:20:06 -0500

    better date formatting for list page

--- assets/scripts/list/list_item.js
+++ assets/scripts/list/list_item.js
@@ -6,7 +6,7 @@
     this._$lineCount = $('<label class="line-count"></label>');
     this._$dateLabel = $('<label class="date-label"></label>');
     this._$codeBlock = $('<pre class="preview-block"></pre>');
-    
+
     this._$info.append(this._$lineCount);
     this._$info.append(this._$dateLabel);
     this._$element.append(this._$info);
@@ -14,15 +14,28 @@
 
     this._$lineCount.text(info.lines + ' line' + (info.lineCount !== 1 ? 's' : ''));
 
-    var date = new Date(0);
-    date.setUTCSeconds(info.postTime);
-    this._$dateLabel.text(date.toString());
+    this._$dateLabel.text(formatTime(info.postTime));
     this._$codeBlock.text(info.head);
   }
 
   ListItem.prototype.element = function() {
     return this._$element;
   };
+
+  function formatTime(epochTime) {
+    var date = new Date(0);
+    date.setUTCSeconds(epochTime);
+
+    var monthNames = ['January', 'February', 'March', 'April', 'May', 'June',
+      'July', 'August', 'September', 'October', 'November', 'December'];
+    var monthName = monthNames[date.getMonth()];
+    var dateInYear = monthName + ' ' + date.getDate();
+    if (date.getFullYear() !== new Date().getFullYear()) {
+      return dateInYear + ', ' + date.getFullYear();
+    } else {
+      return dateInYear;
+    }
+  }
 
   window.app.ListItem = ListItem;
 
