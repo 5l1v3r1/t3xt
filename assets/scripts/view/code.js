b929c445aed757c26973d0069dd007647ea24707
Alex Nichol <unixpickle@gmail.com>
2016-03-11 19:29:27 -0500

    implemented basic line selection in code view

--- assets/scripts/view/code.js
+++ assets/scripts/view/code.js
@@ -2,10 +2,11 @@
 
   function CodeView() {
     this._$code = $('#code');
-    this._generateCodeRows(parseURLLineSet());
+    this._lineSet = parseURLLineSet();
+    this._generateCodeRows();
   }
 
-  CodeView.prototype._generateCodeRows = function(lineSet) {
+  CodeView.prototype._generateCodeRows = function() {
     var lines = window.app.postInfo.content.split('\n');
     for (var i = 0, len = lines.length; i < len; ++i) {
       var line = lines[i];
@@ -17,20 +18,39 @@
       $row.children('.code-text-container').prepend($codeText);
       this._$code.append($row);
 
-      if (lineSet !== null && lineSet.includes(i)) {
-        // TODO: select the line here.
+      if (this._lineSet.includes(i+1)) {
+        $row.addClass('highlighted-line');
       }
+      this._registerRowClick(i+1, $row);
     }
+  };
+
+  CodeView.prototype._registerRowClick = function(index, $row) {
+    $row.click(function() {
+      if (this._lineSet.includes(index)) {
+        this._lineSet.remove(index);
+        $row.removeClass('highlighted-line');
+      } else {
+        this._lineSet.add(index);
+        $row.addClass('highlighted-line');
+      }
+      this._lineSetChanged();
+    }.bind(this));
+  };
+
+  CodeView.prototype._lineSetChanged = function() {
+    window.location.hash = '#' + this._lineSet;
   };
 
   function parseURLLineSet() {
     if (window.location.hash !== '') {
       try {
-        return window.app.LineSet.parse(window.location.hash.substr(1));
+        var lineCount = window.app.postInfo.content.split('\n').length;
+        return window.app.LineSet.parse(window.location.hash.substr(1), lineCount);
       } catch (e) {
       }
     }
-    return null;
+    return new window.app.LineSet({});
   }
 
   window.app.CodeView = CodeView;
