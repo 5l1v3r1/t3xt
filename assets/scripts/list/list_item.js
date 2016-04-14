ca0658827f87cc63b57acc56e599631202e25827
Alex Nichol <unixpickle@gmail.com>
2016-04-14 18:26:39 -0400

    make list entries true links (<a> tags)
    Before, these list entries listened to onclick events. The problem with this behavior is that it does not people to copy the target address, open the link in a new tab using tap-and-hold or control+click, etc.

--- assets/scripts/list/list_item.js
+++ assets/scripts/list/list_item.js
@@ -2,6 +2,10 @@
 
   function ListItem(info) {
     this._$element = $('<li class="list-item"></li>');
+    this._$link = $('<a class="list-link"></a>');
+    this._$link.attr('href', '/view/' + info.secretId);
+    this._$element.append(this._$link);
+
     this._$info = $('<div class="list-item-info"></div>');
     this._$lineCount = $('<label class="line-count"></label>');
     this._$dateLabel = $('<label class="date-label"></label>');
@@ -9,23 +13,17 @@
 
     this._$info.append(this._$lineCount);
     this._$info.append(this._$dateLabel);
-    this._$element.append(this._$info);
-    this._$element.append(this._$codeBlock);
+    this._$link.append(this._$info);
+    this._$link.append(this._$codeBlock);
 
     this._$lineCount.text(info.lines + ' line' + (info.lines !== 1 ? 's' : ''));
 
     this._$dateLabel.text(window.app.formatTime(info.postTime));
     this._$codeBlock.text(info.head);
-
-    this._$element.click(this._view.bind(this, info.secretId));
   }
 
   ListItem.prototype.element = function() {
     return this._$element;
-  };
-
-  ListItem.prototype._view = function(shareId) {
-    window.location = '/view/' + shareId;
   };
 
   window.app.ListItem = ListItem;
